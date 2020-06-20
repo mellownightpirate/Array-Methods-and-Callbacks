e504e77ca813f62234ae457915e9247fe3507cec
mellownightpirate <a@hasan.co>
2020-06-21 00:11:42 +0100

    Task 2 create a function that returns an array of objects

--- index.js
+++ index.js
@@ -46,12 +46,13 @@ let finalWinner2014 = fifaData.filter((game) => {
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
-
-    /* code here */
-
+function getFinals(data) {
+    const years = data.filter(function(item) {
+        return item['Stage'] === 'Final';
+    })
+    return years;
 };
-
+console.log(getFinals(fifaData));
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
 function getYears(/* code here */) {
